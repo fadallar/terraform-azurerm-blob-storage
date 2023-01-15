027465420eb001c905717c668abee45e5ef5806e
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-01-15 08:36:49 +0100

    Issue whith container logging

--- r-logs.tf
+++ r-logs.tf
@@ -12,17 +12,17 @@ module "diagnostics" {
     name_suffix    = var.name_suffix
 }
 
-module "diagnostics_type" {
-  for_each = toset(["blob", "file", "table", "queue"])
-
-    source  = "app.terraform.io/fabbuildingblocks/diagnostics-settings/azurerm"
-    version = "0.1.2"
-
-    resource_id = format("%s/%sServices/default/", azurerm_storage_account.storage.id, each.key)
-
-    logs_destinations_ids = var.logs_destinations_ids
-    log_categories        = var.logs_categories
-    metric_categories     = var.logs_metrics_categories
-    retention_days        = var.logs_retention_days
-    custom_name    = var.custom_diagnostic_settings_name
-}
+#module "diagnostics_type" {
+#  for_each = toset(["blob", "file", "table", "queue"])
+#
+#    source  = "app.terraform.io/fabbuildingblocks/diagnostics-settings/azurerm"
+#    version = "0.1.2"
+#
+#    resource_id = format("%s/%sServices/default/", azurerm_storage_account.storage.id, each.key)
+#
+#    logs_destinations_ids = var.logs_destinations_ids
+#    log_categories        = var.logs_categories
+#    metric_categories     = var.logs_metrics_categories
+#    retention_days        = var.logs_retention_days
+#    custom_name    = var.custom_diagnostic_settings_name
+#}
